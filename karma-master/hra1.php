f93437466561f689bb4417f810a12ad838ab5f09
LeanApa <lea.apagro@gmail.com>
2021-04-27 21:11:50 -0300

    prueba 36 2.0

--- karma-master/hra1.php
+++ karma-master/hra1.php
@@ -244,9 +244,7 @@
 											<i class="fa fa-star"></i>
 										</div>
 									</div>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-										dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-										commodo</p>
+									<p>El Producto Llegó Rápido a provincia y cumplió todas mis espectativas. Gracias.</p>
 								</div>
 								<div class="review_item">
 									<div class="media">
