369f2a216f2f071938db4ff8d221ff461acea470
emanuel004 <71942461+emanuel004@users.noreply.github.com>
2021-04-29 13:31:00 -0300

    ingreso de datos

---
+++ karma-master/datos/categoria.php
@@ -0,0 +1,14 @@
+$categoria = array( 
+    "1" => array(
+            "Id" => 1,	    
+            "nombre" => "zapatillas",	        
+            ),
+    "2" => array(
+            "Id" => 2,
+            "nombre" => "Rinning",
+            ),
+    "3" => array(
+            "Id" => 3,
+            "nombre" => "Trainnig",
+            )
+);
---
+++ karma-master/datos/marcas.php
@@ -0,0 +1,21 @@
+<?php
+$marcas = array( 
+    "1" => array(
+        "Id" => 1,
+        "nombre" => "Addidas"
+    ),
+    "2" => array(
+        "Id" => 2,
+        "nombre" => "Nike"
+    ),
+    "3" => array(
+        "Id" => 3,
+        "nombre" => "Puma"
+    ),
+    "4" => array(
+        "Id" => 4,
+        "nombre" => "Addidas"
+    ),
+);
+
+?>
