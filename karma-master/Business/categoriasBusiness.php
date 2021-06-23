eaea1fe59c081650672ee6c41415a5484c9c38d7
LeanApa <lea.apagro@gmail.com>
2021-06-22 23:55:25 -0300

    cambios

--- karma-master/Business/categoriasBusiness.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('DAO/categoriasDao.php');
-
- 
-function businessObtenerCategorias(){
- 
-    return daoObtenerCategorias();
-
-}
--- karma-master/Business/marcasBusiness.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('DAO/marcasDao.php');
-
- 
-function businessObtenerMarcas(){
- 
-    return daoObtenerMarcas();
-
-}
