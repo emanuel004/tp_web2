9490909e3dd6a6ab6b6ce120cdbb059f09105b67
emanuel004 <71942461+emanuel004@users.noreply.github.com>
2021-06-23 21:33:47 -0300

    cerrando etiquetas

--- karma-master/Business/categoriasBusiness.php
+++ karma-master/Business/categoriasBusiness.php
@@ -7,3 +7,5 @@ function businessObtenerCategorias(){
     return daoObtenerCategorias();
 
 }
+
+?>
--- karma-master/Business/productosBusiness.php
+++ karma-master/Business/productosBusiness.php
@@ -57,3 +57,5 @@ function businessBorrarProducto($ID){
     daoBorrarProducto($ID);
      
 }
+
+?>
